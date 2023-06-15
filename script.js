e29ee291b1b2239349b9409ee6de62709379f084
snmrehar <136726708+snmrehar@users.noreply.github.com>
2023-06-15 15:01:13 +0000

    added

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Access the HTML element with the ID "myButton"
+    var button = document.getElementById("myButton");
+  
+    // Add a click event listener to the button
+    button.addEventListener("click", function() {
+      // Perform an action when the button is clicked
+      alert("Button clicked!");
+    });
+  });
+  
