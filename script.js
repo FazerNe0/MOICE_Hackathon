3d70518230199a711c23c1c8f4436c209e19d717
FazerNeo <99075115+FazerNe0@users.noreply.github.com>
2023-06-15 15:54:59 +0000

    ok

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+// Automatic image slider
+var slideIndex = 0;
+
+function showSlides() {
+  var slides = document.getElementsByClassName("slide");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change slide every 3 seconds (adjust as needed)
+}
+
+// Run the slider function when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function() {
-    // Access the HTML element with the ID "myButton"
-    var button = document.getElementById("myButton");
-  
-    // Add a click event listener to the button
-    button.addEventListener("click", function() {
-      // Perform an action when the button is clicked
-      alert("Button clicked!");
-    });
-  });
-  
+  showSlides();
+});
